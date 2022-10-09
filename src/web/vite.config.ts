7d6beb908f633948d69620ff0967242d24fb7bd5
Leo Shchurov <leonid.shchurov@gmail.com>
2022-10-09 17:11:36 +1100

    Fixed local vite config for paths

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -1,8 +1,19 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@prisma/client/runtime/': '@prisma/client/runtime/',
+      '@prisma/': path.join(__dirname, '../../node_modules/.prisma/'),
+
+      'pages/': path.join(__dirname, 'src/pages/'),
+      'components/': path.join(__dirname, 'src/components/'),
+      'routes/': path.join(__dirname, 'src/routes/'),
+      'utils/': path.join(__dirname, 'src/utils/'),
+    },
+  },
 });
