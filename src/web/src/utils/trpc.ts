8cfa14d6b2129475d7d5b16a0ee209264e828492
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-08-31 15:15:12 +1000

    fix up file structure and imports

--- src/web/src/utils/trpc.ts
+++ src/web/src/utils/trpc.ts
@@ -1,5 +1,5 @@
 // utils/trpc.ts
-import type { AppRouter } from '@server/server';
 import { createTRPCReact } from '@trpc/react-query';
+import type { AppRouter } from 'server/src/server'; // This is a type so it can be imported during development
 
 export const trpc = createTRPCReact<AppRouter>();
