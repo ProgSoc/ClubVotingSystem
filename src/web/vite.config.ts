74595511a7286e65947ed03fde66aa16bc62f607
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-08-31 18:36:12 +1000

    vite-tsconfig-paths

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -1,20 +1,9 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@prisma/client/runtime/': '@prisma/client/runtime/',
-      '@prisma/': path.join(__dirname, '../../node_modules/.prisma/'),
-      '@server/': path.join(__dirname, '../server/src/'),
-
-      'pages/': path.join(__dirname, 'src/pages/'),
-      'components/': path.join(__dirname, 'src/components/'),
-      'routes/': path.join(__dirname, 'src/routes/'),
-      'utils/': path.join(__dirname, 'src/utils/'),
-    },
-  },
+  plugins: [react(), viteTsConfigPaths()],
 });
