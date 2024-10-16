b35c52d9f4563e8d13f9e1da6db7cdcf697f1ac5
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2024-10-16 19:12:17 +1100

    comments

--- src/server/src/room/interaction/preferentialVote.test.ts
+++ src/server/src/room/interaction/preferentialVote.test.ts
@@ -8,6 +8,9 @@ describe('Ranked Choice Voting', () => {
     const maxElected = 1;
 
     const result = rankedChoiceVoting(candidates, votes, maxElected);
+    // first round input is a,b,a
+    // b is eliminated
+    // second round input is a,a
 
     expect(result).toEqual([{ id: 'A', votes: 2 }]);
   });
@@ -18,6 +21,9 @@ describe('Ranked Choice Voting', () => {
     const maxElected = 1;
 
     const result = rankedChoiceVoting(candidates, votes, maxElected);
+    // first round input is a,b,a,b
+    // a and b are tied, but we only want to elect 1 so we don't go to the second round
+    // so the result is a tie
 
     expect(result).toEqual([{ id: 'A', votes: 2 }, { id: 'B', votes: 2 }]);
   });
@@ -28,6 +34,9 @@ describe('Ranked Choice Voting', () => {
     const maxElected = 1;
 
     const result = rankedChoiceVoting(candidates, votes, maxElected);
+    // first round input is a,b,c,c
+    // b and a are eliminated
+    // second round input is c,c,c
 
     expect(result).toEqual([{ id: 'C', votes: 3 }]);
   });
@@ -38,6 +47,11 @@ describe('Ranked Choice Voting', () => {
     const maxElected = 1;
 
     const result = rankedChoiceVoting(candidates, votes, maxElected);
+    // first round input is a,b,c,c,a
+    // b is eliminated
+    // second round input is a,c,c,c,a
+    // a is eliminated
+    // third round input is c,c,c
 
     expect(result).toEqual([{ id: 'C', votes: 3 }]);
   });
@@ -48,7 +62,38 @@ describe('Ranked Choice Voting', () => {
     const maxElected = 1;
 
     const result = rankedChoiceVoting(candidates, votes, maxElected);
+    // c is removed
+    // first round input is b,a,a
+    // b is removed
+    // second round input is a,a
 
     expect(result).toEqual([{ id: 'A', votes: 2 }]);
+  });
+
+  // edge cases
+  it('elects all candidates when there are no votes', () => {
+    const candidates = ['A', 'B'];
+    const votes: string[][] = [];
+    const maxElected = 1;
+
+    const result = rankedChoiceVoting(candidates, votes, maxElected);
+
+    expect(result).toEqual([{ id: 'A', votes: 0 }, { id: 'B', votes: 0 }]);
+  });
+
+  // handle multiple candidates being elected
+  it('elects all candidates when there are enough votes', () => {
+    const candidates = ['A', 'B', 'C'];
+    const votes = [['A', 'B'], ['B', 'C'], ['C', 'A'], ['C', 'A'], ['A', 'B'], ['A', 'B']];
+    const maxElected = 2;
+
+    const result = rankedChoiceVoting(candidates, votes, maxElected);
+    // first round input is a,b,c,c,a,a
+    // b is eliminated
+    // second round input is a,c,c,c,a,a
+    // c is eliminated
+    // third round input is a,a,a
+
+    expect(result).toEqual([{ id: 'A', votes: 3 }, { id: 'C', votes: 3 }]); // A and C are tied
   });
 });
