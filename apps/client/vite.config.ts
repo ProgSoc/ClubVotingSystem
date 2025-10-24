a46b518cd3f0ff5efb7f7ae752ea014f9d07fd88
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-10-24 19:02:04 +1100

    update tailwind and daisyui

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -6,6 +7,7 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		viteTsconfigPaths(),
 		tanstackRouter({
 			target: "react",
